250b6796e800de1c61a139a471affca499d634fd
Krifty <kaitlynmariejones@gmail.com>
2022-01-11 16:40:14 -0800

    style

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
 import _ from 'lodash';
-
-function componentTEST() {
-  const element = document.createElement('div');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(componentTEST());
+import "./style/style.css";
